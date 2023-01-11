588c8ed2273375fd61bcf8c1a399315fca956eaa
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-01-11 15:33:56 +0100

    Add CSS rules to Box

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
-import { Box, Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { Box, Typography, CardContent, CardMedia } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail }) => (
-  <Box sx={{ boxShadow: "none", borderRadius: "20px" }}>
+const ChannelCard = ({ channelDetail, marginTop }) => (
+  <Box
+    sx={{
+      boxShadow: "none",
+      borderRadius: "20px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: { xs: "356px", md: "320px" },
+      height: "326px",
+      margin: "auto",
+      marginTop,
+    }}
+  >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
       <CardContent
         sx={{
